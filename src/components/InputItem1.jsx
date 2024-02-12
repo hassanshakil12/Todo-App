80b08f412f8eba1d1ee7a0f2df59b902ae232f59
Muhammad Hassan <hassanshakil12@gmail.com>
2024-02-12 23:26:33 +0500

    Converted to dynamic components

--- src/components/InputItem1.jsx
+++ src/components/InputItem1.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const InputItem1 = () => {
+  let todoName = 'Buy Vegetables';
+  let todoDate = '12/02/2024';
   return (
     <div className="container">
         <div className="row Row">
-            <div className="col-6">Buy Vegetables</div>
-            <div className="col-4">12/02/2024</div>
+            <div className="col-6">{todoName}</div>
+            <div className="col-4">{todoDate}</div>
             <div className="col-2 text-center">
                 <div className="btn btn-danger Btn">Delete</div>
             </div>
--- src/components/InputItem2.jsx
+++ src/components/InputItem2.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 const InputItem2 = () => {
+  let todoName = 'Go To University';
+  let todoDate = '19/02/2024';
   return (
     <div className="container">
         <div className="row Row">
-            <div className="col-6">Go To University</div>
-            <div className="col-4">19/02/2024</div>
+            <div className="col-6">{todoName}</div>
+            <div className="col-4">{todoDate}</div>
             <div className="col-2 text-center">
                 <div className="btn btn-danger Btn">Delete</div>
             </div>
