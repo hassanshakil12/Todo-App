c8bd66c12b1f20c593ba521de097e3c13f670e88
Muhammad Hassan <hassanshakil12@gmail.com>
2024-02-14 01:53:44 +0500

    Added Modular CSS

--- src/components/InputComp.jsx
+++ src/components/InputComp.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import css from "../styles/InputComp.module.css"
 
 const InputComp = () => {
   return (
-    <div className="container">
-        <div className="row Row">
+    <div className={`${css.container} container`}>
+        <div className={`${css.Row} row`}>
             <div className="col-6">
-                <input type="text" placeholder='Enter todo task'/>
+                <input type="text" placeholder='Enter todo task' className={`${css.Input}`}/>
             </div>
             <div className="col-4">
-                <input type="date" />
+                <input type="date" className={`${css.Input}`}/>
             </div>
             <div className="col-2 text-center">
-                <div className="btn btn-success Btn">Add</div>
+                <div className={`${css.Btn} btn btn-success`}>Add</div>
             </div>
         </div>
     </div>
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import css from "../styles/TodoItems.module.css";
 
 const InputItem2 = ({name, date}) => {
   return (
-    <div className="container">
-      <div className="row Row">
+    <div className={`${css.container} container`}>
+      <div className={`${css.Row} row`}>
         <div className="col-6">{name}</div>
         <div className="col-4">{date}</div>
         <div className="col-2 text-center">
-          <div className="btn btn-danger Btn">Delete</div>
+          <div className={`${css.Btn} btn btn-danger`}>Delete</div>
         </div>
       </div>
     </div>
